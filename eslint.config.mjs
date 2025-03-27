f2749ccc93ca01f839bb6e42ed59ebfeee6c142f
peternhale <peter.hale@salesforce.com>
2025-03-27 11:17:38 -0600

    chore: wip

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,57 +4,69 @@
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
-import typescriptEslintPlugin from "@typescript-eslint/eslint-plugin";
-import prettierPlugin from "eslint-plugin-prettier";
-import importPlugin from "eslint-plugin-import";
-import unusedImportsPlugin from "eslint-plugin-unused-imports";
-import jsdocPlugin from "eslint-plugin-jsdoc";
+import typescriptEslintPlugin from '@typescript-eslint/eslint-plugin';
+import prettierPlugin from 'eslint-plugin-prettier';
+import importPlugin from 'eslint-plugin-import';
+import unusedImportsPlugin from 'eslint-plugin-unused-imports';
+import jsdocPlugin from 'eslint-plugin-jsdoc';
+import header from '@tony.ganchev/eslint-plugin-header';
 import typescriptParser from "@typescript-eslint/parser";
-import header from "@tony.ganchev/eslint-plugin-header";
 
 export default [
   {
-    files: ["**/*.ts", "**/*.mjs"],
+    files: ['**/*.ts', '**/*.mjs'],
     languageOptions: {
       ecmaVersion: 2021,
-      sourceType: "module",
+      sourceType: 'module',
       parser: typescriptParser,
       globals: {
         // Add any global variables here if needed
-        console: "readonly",
-        process: "readonly",
+        console: 'readonly',
+        process: 'readonly',
       },
     },
     plugins: {
-      "@typescript-eslint": typescriptEslintPlugin,
+      '@typescript-eslint': typescriptEslintPlugin,
       header: header,
       prettier: prettierPlugin,
       import: importPlugin,
-      "unused-imports": unusedImportsPlugin,
+      'unused-imports': unusedImportsPlugin,
       jsdoc: jsdocPlugin,
     },
     rules: {
-      "prettier/prettier": "error",
-      "no-unused-vars": "off",
-      "@typescript-eslint/no-unused-vars": ["error"],
-      "arrow-body-style": ["error", "as-needed"],
-      "unused-imports/no-unused-imports": "error",
-      "jsdoc/check-alignment": "warn",
-      "jsdoc/check-indentation": "warn",
-      "header/header": [
-        "error",
-        "block",
+      'prettier/prettier': 'error',
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': ['error'],
+      'arrow-body-style': ['error', 'as-needed'],
+      'unused-imports/no-unused-imports': 'error',
+      'jsdoc/check-alignment': 'warn',
+      'jsdoc/check-indentation': 'warn',
+      'header/header': [
+        'error',
+        'block',
         [
-          "",
+          '',
           {
-            pattern: " \\* Copyright \\(c\\) \\d{4}, salesforce\\.com, inc\\.",
-            template: " * Copyright (c) 2025, salesforce.com, inc.",
+            pattern: ' \\* Copyright \\(c\\) \\d{4}, salesforce\\.com, inc\\.',
+            template: ' * Copyright (c) 2025, salesforce.com, inc.',
           },
-          " * All rights reserved.",
-          " * Licensed under the BSD 3-Clause license.",
-          " * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause",
-          " ",
+          ' * All rights reserved.',
+          ' * Licensed under the BSD 3-Clause license.',
+          ' * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause',
+          ' ',
         ],
+      ],
+      quotes: ['error', 'single'],
+      'import/no-unresolved': 'error',
+      'import/named': 'error',
+      'import/default': 'error',
+      'import/no-duplicates': 'error',
+      'import/order': [
+        'error',
+        {
+          groups: ['builtin', 'external', 'internal'],
+          'newlines-between': 'always',
+        },
       ],
     },
   },
--- packages/extension-apex-ls-ts/src/index.ts
+++ packages/extension-apex-ls-ts/src/index.ts
@@ -1,30 +1,36 @@
+/*
+ * Copyright (c) 2025, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
 import {
   createConnection,
   ProposedFeatures,
   InitializeParams,
   InitializeResult,
-} from "vscode-languageserver/node";
+} from 'vscode-languageserver/node';
 
 // Create a connection for the server. The connection uses Node's IPC as a transport.
 const connection = createConnection(ProposedFeatures.all);
 
 // Initialize server capabilities and properties
-connection.onInitialize((params: InitializeParams): InitializeResult => {
-  return {
+connection.onInitialize(
+  (params: InitializeParams): InitializeResult => ({
     capabilities: {
       textDocumentSync: 1, // Full synchronization
       completionProvider: {
         resolveProvider: true,
-        triggerCharacters: ["."],
+        triggerCharacters: ['.'],
       },
       hoverProvider: true,
     },
-  };
-});
+  }),
+);
 
 // Handle client connection
 connection.onInitialized(() => {
-  console.log("Language server initialized and connected to client.");
+  console.log('Language server initialized and connected to client.');
 });
 
 // Listen on the connection
