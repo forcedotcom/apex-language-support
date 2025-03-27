1781702bcba0026bd20bb34367d012631f08d090
peternhale <peter.hale@salesforce.com>
2025-03-27 11:39:51 -0600

    chore: setup for jest testing

---
+++ jest.config.mjs
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2025, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+
+import { pathsToModuleNameMapper } from 'ts-jest';
+
+import { compilerOptions } from './tsconfig.json';
+
+export default {
+  transform: {},
+  extensionsToTreatAsEsm: ['.ts'],
+  testEnvironment: 'node',
+  testMatch: ['**/test/**/*.test.ts'],
+  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+  rootDir: './',
+  roots: ['<rootDir>/packages'],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths || {}, {
+    prefix: '<rootDir>/',
+  }),
+};
