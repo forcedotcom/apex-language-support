ca104ee931da596f59ce3803e299b574386674f3
peternhale <peter.hale@salesforce.com>
2025-05-30 11:27:06 -0600

    chore: apply review suggestions

--- packages/apex-parser-ast/test/setup.ts
+++ packages/apex-parser-ast/test/setup.ts
@@ -1,5 +1,13 @@
-import { TestLogger } from './utils/testLogger';
+/*
+ * Copyright (c) 2025, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the
+ * repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
 import { LogLevel } from '@salesforce/apex-lsp-logging';
+
+import { TestLogger } from './utils/testLogger';
 
 // Configure test logger for all tests
 const logger = TestLogger.getInstance();
