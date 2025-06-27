b9e083c8831b5880c904d04123cd497202a68b3b
peternhale <peter.hale@salesforce.com>
2025-06-27 11:10:07 -0600

    chore: move pjson manipulation to tsup on success

--- packages/apex-lsp-vscode-extension-web/update-package.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-
-const packagePath = path.join(__dirname, 'out', 'package.json');
-const pkg = JSON.parse(fs.readFileSync(packagePath, 'utf8'));
-
-pkg.main = './extension.js';
-
-fs.writeFileSync(packagePath, JSON.stringify(pkg, null, 2));
