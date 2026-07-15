aeb8b7648c60f4f18aa11cf47cf7e6b5fff3bc31
Kyle Walker <kyle.walker@salesforce.com>
2026-07-15 13:03:00 -0700

    feat(repo): declare missing method quickfix code action (multi-file) - W-23389336

--- packages/apex-parser-ast/src/index.ts
+++ packages/apex-parser-ast/src/index.ts
@@ -73,6 +73,7 @@ export * from './utils/ApexKeywords';
 export * from './utils/effectUtils';
 export * from './utils/UnifiedCache';
 export * from './utils/expressionRangeFinder';
+export * from './utils/methodCallAtRange';
 
 // Export cross-file symbol management
 export * from './symbols/ApexSymbolManager';
